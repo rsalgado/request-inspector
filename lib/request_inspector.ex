d6dfe5b71a17e92f8e450b6f4e1f83a27bf4eb6f
Roberto Salgado <rsalgado03@gmail.com>
2018-06-06 14:38:40 -0500

    Reorder the children of the application's main supervisor

--- lib/request_inspector.ex
+++ lib/request_inspector.ex
@@ -7,9 +7,9 @@ defmodule RequestInspector do
 
   def start(_type, _args) do
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, RequestInspector.Router, [], port: 5000),
       RequestsAgent.child_spec([]),
-      StreamAgent.child_spec([])
+      StreamAgent.child_spec([]),
+      Plug.Adapters.Cowboy.child_spec(:http, RequestInspector.Router, [], port: 5000)
     ]
 
     Logger.info("Starting server on port 5000")
