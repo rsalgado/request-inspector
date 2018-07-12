c38ede081d2d258d50fc097fb0541d4f1a32f7d8
Roberto Salgado <rsalgado03@gmail.com>
2018-07-12 11:14:46 -0500

    Modify Cowboy2 adapter options to avoid idle connections getting closed after 60s

--- lib/request_inspector.ex
+++ lib/request_inspector.ex
@@ -10,9 +10,20 @@ defmodule RequestInspector do
     # Use the modules names as agents' names. 
     # Also used in `RequestInspector.Router` attributes
     children = [
-      {Plug.Adapters.Cowboy2, scheme: :http, plug: RequestInspector.Router, options: [port: 5000]},
-      Registry.child_spec(keys: :unique, name: @registry),
-      {DynamicSupervisor, strategy: :one_for_one, name: @dynamic_supervisor}
+      Registry.child_spec(
+        keys: :unique,
+        name: @registry),
+      {DynamicSupervisor,
+        strategy: :one_for_one,
+        name: @dynamic_supervisor},
+      {Plug.Adapters.Cowboy2,
+        scheme: :http,
+        plug: RequestInspector.Router,
+        options: [
+          port: 5000,
+          # Using :infinity because Cowboy2 by default waits 60s before closing the conn.
+          # TODO: Find a better way to deal with SSE in Cowboy2 using Plug to avoid this workaround.
+          protocol_options: [idle_timeout: :infinity]]}
     ]
 
     Logger.info("Starting server on port 5000")
